35adb5df9a02fc9cd6b900a5a8adfc338cb9ba9a
Donika <marchevadonika@gmail.com>
2023-11-19 16:27:15 +0200

    add appropriate css to register form

--- client/src/components/Register/Register.jsx
+++ client/src/components/Register/Register.jsx
@@ -1,5 +1,25 @@
+import styles from './Register.module.css'
+
+import {Link} from 'react-router-dom'
+
 export default function Register() {
     return (
-        <h1>Register</h1>
+        <div className={styles.register}>
+            <div className={styles.container}>
+                <div className={styles["login-form"]}>
+                    <h2 className={styles.active}> register </h2>
+                    <form>
+                        <input type="text" className={styles.text} name="username" />
+                        <span>username</span><br /><br />
+                        <input type="password" className={styles.text} name="password" />
+                        <span>password</span><br />
+                        <input type="password" className={styles.text} name="repeat-password" />
+                        <span>repeat password</span><br />
+                        <button className={styles.signin}>Register</button>
+                        <Link to={'/register'}>You already have an account?</Link>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
