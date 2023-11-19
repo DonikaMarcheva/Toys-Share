4f4977f526ce2cb0b0671456e59aec624f6ecabe
Donika <marchevadonika@gmail.com>
2023-11-19 15:41:48 +0200

    add background picture and put css

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,5 +1,23 @@
+import styles from './Login.module.css'
+
+import {Link} from 'react-router-dom'
+
 export default function Login() {
     return (
-        <h1>Login</h1>
+        <div className={styles.login}>
+            <div className={styles.container}>
+                <div className={styles["login-form"]}>
+                    <h2 className={styles.active}> sign in </h2>
+                    <form>
+                        <input type="text" className={styles.text} name="username" />
+                        <span>username</span><br /><br />
+                        <input type="password" className={styles.text} name="password" />
+                        <span>password</span><br />
+                        <button className={styles.signin}>Sign In</button>
+                        <Link to={'/register'}>You dont have an account?</Link>
+                    </form>
+                </div>
+            </div>
+        </div>
     );
 }
