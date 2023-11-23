967365d17070ac186d815d3dc4b81c12feda0608
Donika <marchevadonika@gmail.com>
2023-11-23 15:39:24 +0200

    add authService

---
+++ client/src/services/authService.js
@@ -0,0 +1,22 @@
+import * as request from '../lib/request.js'
+
+const baseUrl= 'http://localhost:3030/users'
+
+export const login =  (email, password) => {
+    const result = request.post(`${baseUrl}/login`, {
+        email,
+        password
+    })
+
+    return result;
+}
+export const register = (email, password) => {
+    const result =  request.post(`${baseUrl}/register`, {
+        email,
+        password
+    })
+
+    return result;
+}
+
+export const logout = () => request.get(`${baseUrl}/logout`);
