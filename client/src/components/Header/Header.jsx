563304da7fe8f0af0ebdf98f9560d92191818cc7
Donika <marchevadonika@gmail.com>
2023-11-23 15:36:22 +0200

    update navigation bar with the new paths

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 
 import logo from './logo.png'
 import styles from './Header.module.css'
+import Path from '../../path.js';
 
 export default function Header() {
     return (
@@ -17,19 +18,19 @@ export default function Header() {
 
                     <div className={styles.navigation}>
                         <ul>
-                            <li><Link to="/about">About</Link></li>
-                            <li><Link to="/services">Services</Link></li>
-                            <li><Link to="/catalog">Catalog</Link></li>
+                            <li><Link to={Path.About}>About</Link></li>
+                            <li><Link to={Path.Services}>Services</Link></li>
+                            <li><Link to={Path.Catalog}>Catalog</Link></li>
                             <li className={styles.user}><Link to="/add">Add Toys</Link></li>
                         </ul>
                     </div>
                     <div className={styles.user}>
-                        <Link to="/logout">Logout</Link>
+                        <Link to={Path.Logout}>Logout</Link>
                     </div>
 
                     <div className={styles.guest}>
-                        <Link to="/login">Login</Link>
-                        <Link to="/register">Register</Link>
+                        <Link to={Path.Login}>Login</Link>
+                        <Link to={Path.Register}>Register</Link>
                     </div>
 
                 </div>
