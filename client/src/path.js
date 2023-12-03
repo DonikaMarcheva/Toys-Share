fe283455639e942d8cf8a0cbe2d5c62fee28c081
Donika <marchevadonika@gmail.com>
2023-12-03 17:25:59 +0200

    add path to map container

--- client/src/path.js
+++ client/src/path.js
@@ -5,6 +5,7 @@ const Path ={
     Edit:"/catalog/edit/:toyId",
     Catalog:"/catalog",
     ToyDetails:`/catalog/:toyId`,
+    Map:`/catalog/:toyId/:toyAddress`,
     Services:"/services",
     Login:"/login",
     Register:"/register",
