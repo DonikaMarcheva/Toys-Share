f4b890f5ae236e500155fc4c896b0ac1effc5240
Donika <marchevadonika@gmail.com>
2023-12-09 01:32:39 +0200

    delete debuger

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -46,6 +46,7 @@ export default function useForm(submitHandler, initialValues) {
 
     const onSubmit = (e) => {
         e.preventDefault();
+        debugger
         submitHandler(values);
         setValues(initialValues);
     };
