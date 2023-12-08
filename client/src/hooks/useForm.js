471e39b28d7487cb7a70ed10c076ed779d0101e0
Donika <marchevadonika@gmail.com>
2023-12-08 23:24:06 +0200

    add validation fields logic

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,13 +1,47 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 export default function useForm(submitHandler, initialValues) {
 
     const [values, setValues] = useState(initialValues);
+    const [errors, setErrors] = useState(initialValues);
+    const [isValid, setIsValid]=useState(false);
 
     const onChange = (e) => {
         setValues(state => ({
             ...state, [e.target.name]: e.target.value
         }))
+
+        for (const key in values) {
+            if (values[key].length === 0) {
+                setErrors(state => ({
+                    ...state, [key]: `${key} required`
+                }))
+            } else {
+                if (key === 'email' && !/[A-Za-z1-9._]+@[a-z]+\.[a-z]+/.test(values[key])) {
+                    setErrors(state => ({
+                        ...state, [key]: `${key} must be valid`
+                    }))
+                }
+                else if (key === 'password' && values[key].length < 6) {
+                    setErrors(state => ({
+                        ...state, [key]: `${key} must be min 6 characters`
+                    }))
+                } else if (key === 'repeat-password' && values[key] !== values['password']) {
+                    setErrors(state => ({
+                        ...state, [key]: `Passwords don\'t match`
+                    }))
+                }
+                else {
+                    setErrors(state => ({
+                        ...state, [key]: 'ok'
+                    }))
+                }
+            }
+        }
+        const a=Object.values(errors).filter(x=>x!=='ok')
+        if(a.length===0){
+            setIsValid(true);
+        }
     }
 
     const onSubmit = (e) => {
@@ -16,14 +50,10 @@ export default function useForm(submitHandler, initialValues) {
         setValues(initialValues);
     };
 
-    const changeValues = (newValues) => {
-        setValues(newValues);
-    };
-
-    return {
+        return {
         values,
+        errors,
         onChange,
         onSubmit,
-        changeValues
     }
 }
