01fbdf54bd823bf97f583e186a3131b12671d18a
Donika <marchevadonika@gmail.com>
2023-12-08 23:28:58 +0200

    remove validation fileds logic
    remove alerts for incorrect register field data and all the logic for this

--- client/src/contexts/authContext.jsx
+++ client/src/contexts/authContext.jsx
@@ -2,7 +2,6 @@ import { createContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useLocalStorage } from '../hooks/useLocalStorage.js';
 
-// import { authServiceFactory } from '../services/authService';
 import * as authService from '../services/authService.js'
 import Path from '../path.js';
 
@@ -14,8 +13,6 @@ export const AuthProvider = ({
     const [auth, setAuth] = useLocalStorage('auth', {});
     const navigate = useNavigate();
 
-    // const authService = authServiceFactory(auth.accessToken)
-
     const loginSubmitHandler = async (values) => {
 
         const result = await authService.login(values.email, values.password);
@@ -26,15 +23,6 @@ export const AuthProvider = ({
 
     };
     const registerSubmitHandler = async (values) => {
-        if (!values.email || !values.username || !values.password || !values["repeat-password"]) {
-            alert('All fields are required!');
-            return;
-        }
-        if (values.password !== values["repeat-password"]) {
-            alert('Passwords don\'t match!');
-            return;
-        }
-
 
         const result = await authService.register(values.email, values.username, values.password);
         setAuth(result);
