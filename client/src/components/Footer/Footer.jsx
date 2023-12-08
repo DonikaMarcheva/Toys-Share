aecfd22807b854430dce4d4c482bfd3b304dfa0e
Donika <marchevadonika@gmail.com>
2023-12-09 01:28:32 +0200

    add some footer features

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareFacebook, faSquareInstagram, faLinkedin, faYoutube } from '@fortawesome/free-brands-svg-icons'
+
 import styles from './Footer.module.css'
+import Path from '../../path.js';
 
 export default function Footer() {
     return (
         <div className={styles["site-footer"]}>
-            <p>Copyright © 2023 Donika®</p>
+            {/* <div className={styles.container}> */}
+            <div className={styles["footer-content"]}>
+                <Link to= {Path.Terms}>Terms and condition</Link>
+                <p><FontAwesomeIcon icon={faSquareFacebook}
+                    style={{ color: "#cc804d", padding: '0.2em 0.5em', marginRight: '0.5em' }} />
+                <FontAwesomeIcon icon={faSquareInstagram}
+                    style={{ color: "#cc804d", padding: '0.2em 0.5em', marginRight: '0.5em' }} />
+                <FontAwesomeIcon icon={faLinkedin}
+                    style={{ color: "#cc804d", padding: '0.2em 0.5em', marginRight: '0.5em' }} />
+                <FontAwesomeIcon icon={faYoutube}
+                    style={{ color: "#cc804d", padding: '0.2em 0.5em', marginRight: '0.5em' }} />
+                    
+                    </p>
+            </div>
+            <div className={styles["copyrights-wrapper"]}>
+                <p className={styles.copyrights}>Copyright © 2023 Donika®</p>
+            </div>
+            {/* </div> */}
         </div>
     );
+
 }
---
+++ client/src/components/Terms/Terms.jsx
@@ -0,0 +1,7 @@
+export default function Terms(){
+    return(
+        <>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas illo id iste? Eligendi tempora quasi fuga, quis repellendus dolore ex illum, neque ipsum debitis in aliquam veritatis, vitae sequi fugit!</p>
+        </>
+    );
+}
